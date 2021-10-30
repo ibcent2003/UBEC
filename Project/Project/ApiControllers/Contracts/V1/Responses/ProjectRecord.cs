c6ce37261efa825f7ea2e0eb258f932ec9eb0073
teqcenta <teqcenta@gmail.com>
2021-10-30 04:08:21 +0000

    Inspection API Implementation

---
+++ Project/Project/ApiControllers/Contracts/V1/Responses/ProjectRecord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project.ApiControllers.Contracts.V1.Responses
+{
+    public class ProjectRecord
+    {
+        public int ProjectId { get; set; }
+        public string SerialNo { get; set; }
+        public string ProjectType { get; set; }
+        public string Contractor { get; set; }
+        public string Description { get; set; }
+        public string OwnedBy { get; set; }
+    }
+}
